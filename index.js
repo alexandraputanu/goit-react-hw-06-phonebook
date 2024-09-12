b6afe79f105285809f25f5ebfcac3eae7342f918
Alexandra Putanu <alexandra_putanu@yahoo.com>
2024-09-12 11:06:29 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+import './index.css';
+import { Provider as ReduxProvider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './redux/store';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ReduxProvider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </ReduxProvider>
+);
